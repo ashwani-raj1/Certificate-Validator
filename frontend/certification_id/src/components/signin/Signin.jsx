4eed23bf2378e9425b98c6240c52629da93c8205
ashwani-sinha <ashwaniraj084@gmail.com>
2026-09-03 22:51:51 +0530

    signin updated

--- frontend/certification_id/src/components/signin/Signin.jsx
+++ frontend/certification_id/src/components/signin/Signin.jsx
@@ -15,7 +15,7 @@ function Signin() {
 
         try {
 
-            const res = await fetch("http://localhost:5000/signin", {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/signin`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
